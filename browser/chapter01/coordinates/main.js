f7802aa4d5c95a99b9fb8e60f32afeac6a2e7db4
progyu <rocket7101@gmail.com>
2020-11-21 18:19:41 +0900

    refactor: Improve performance

--- browser/chapter01/coordinates/main.js
+++ browser/chapter01/coordinates/main.js
@@ -3,15 +3,24 @@ const vertical = document.querySelector('.vertical')
 const target = document.querySelector('.target')
 const tag = document.querySelector('.tag')
 
-document.addEventListener('mousemove', event => {
-  const x = event.clientX
-  const y = event.clientY
+addEventListener('load', () => {
+  const rect = target.getBoundingClientRect()
+  const targetHalfWidth = rect.width / 2
+  const targetHalfHeight = rect.height / 2
 
-  vertical.style.left = `${x}px`
-  horizontal.style.top = `${y}px`
-  target.style.left = `${x}px`
-  target.style.top = `${y}px`
-  tag.style.left = `${x}px`
-  tag.style.top = `${y}px`
-  tag.innerHTML = `${x}px, ${y}px`
+  document.addEventListener('mousemove', event => {
+    const x = event.clientX
+    const y = event.clientY
+
+    vertical.style.transform = `translateX(${x}px)`
+    horizontal.style.transform = `translateY(${y}px)`
+
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px)`
+
+    tag.style.transform = `translate(${x}px, ${y}px)`
+
+    tag.innerHTML = `${x}px, ${y}px`
+  })
 })
