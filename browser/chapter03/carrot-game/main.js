34e8d4311c6880136814b235e3a31b7de0a7b543
progyu <rocket7101@gmail.com>
2020-12-02 01:39:57 +0900

    feat: start game

--- browser/chapter03/carrot-game/main.js
+++ browser/chapter03/carrot-game/main.js
@@ -1,12 +1,73 @@
 'use strict'
 
+const CARROT_SIZE = 80
+const CARROT_COUNT = 5
+const BUG_COUNT = 5
+const GAME_DURATION_SEC = 5
+
 const field = document.querySelector('.game__field')
 const fieldRect = field.getBoundingClientRect()
-const CARROT_SIZE = 80
+const gameBtn = document.querySelector('.game__button')
+const gameTimer = document.querySelector('.game__timer')
+const gameScore = document.querySelector('.game__score')
+
+let started = false
+let score = 0
+let timer = undefined
+
+gameBtn.addEventListener('click', () => {
+  if (started) {
+    stopGame()
+  } else {
+    startGame()
+  }
+  started = !started
+})
+
+function startGame() {
+  initGame()
+  showStopButton()
+  showTimerAndScore()
+  startGameTimer()
+}
+
+function stopGame() {}
+
+function showStopButton() {
+  const icon = gameBtn.querySelector('.fa-play')
+  icon.classList.add('fa-stop')
+  icon.classList.remove('fa-play')
+}
+
+function showTimerAndScore() {
+  gameTimer.style.visibility = 'visible'
+  gameScore.style.visibility = 'visible'
+}
+
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC
+  updateTimerText(remainingTimeSec)
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer)
+      return
+    }
+    updateTimerText(--remainingTimeSec)
+  }, 1000)
+}
+
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60
+  console.log(seconds)
+  gameTimer.textContent = `${minutes}:${seconds}`
+}
 
 function initGame() {
-  addItem('carrot', 5, 'img/carrot.png')
-  addItem('bug', 5, 'img/bug.png')
+  field.innerHTML = ''
+  gameScore.textContent = CARROT_COUNT
+  addItem('carrot', CARROT_COUNT, 'img/carrot.png')
+  addItem('bug', BUG_COUNT, 'img/bug.png')
 }
 
 function addItem(className, count, imgPath) {
@@ -30,5 +91,3 @@ function addItem(className, count, imgPath) {
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min
 }
-
-initGame()
