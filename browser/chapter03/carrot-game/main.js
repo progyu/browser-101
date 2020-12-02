3b533a2e45ba6e966a2e0ce5a37202e25431250a
progyu <rocket7101@gmail.com>
2020-12-02 16:48:47 +0900

    feat: add sound and fix some bugs

--- browser/chapter03/carrot-game/main.js
+++ browser/chapter03/carrot-game/main.js
@@ -15,6 +15,12 @@ const popup = document.querySelector('.pop-up')
 const popupText = document.querySelector('.pop-up__message')
 const popupRefresh = document.querySelector('.pop-up__refresh')
 
+const carrotSound = new Audio('./sound/carrot_pull.mp3')
+const alertSound = new Audio('./sound/alert.wav')
+const bgSound = new Audio('./sound/bg.mp3')
+const bugSound = new Audio('./sound/bug_pull.mp3')
+const winSound = new Audio('./sound/game_win.mp3')
+
 let started = false
 let score = 0
 let timer = undefined
@@ -33,12 +39,12 @@ popupRefresh.addEventListener('click', () => {
 })
 
 function startGame() {
-  score = 0
   started = true
   initGame()
   showStopButton()
   showTimerAndScore()
   startGameTimer()
+  playSound(bgSound)
 }
 
 function stopGame() {
@@ -46,10 +52,20 @@ function stopGame() {
   stopGameTimer()
   hideGameButton()
   showPopUpWithText('REPLAY❓')
+  playSound(alertSound)
+  stopSound(bgSound)
 }
 
 function finishGame(win) {
-  stopGame()
+  started = false
+  hideGameButton()
+  if (win) {
+    playSound(winSound)
+  } else {
+    playSound(bugSound)
+  }
+  stopGameTimer()
+  stopSound(bgSound)
   showPopUpWithText(win ? 'YOU WON 🎉' : 'YOU LOST 🙈')
 }
 
@@ -102,6 +118,7 @@ function hidePopUp() {
 }
 
 function initGame() {
+  score = 0
   field.innerHTML = ''
   gameScore.textContent = CARROT_COUNT
   addItem('carrot', CARROT_COUNT, 'img/carrot.png')
@@ -116,14 +133,23 @@ function onFieldClick(event) {
   if (target.matches('.carrot')) {
     target.remove()
     score++
+    playSound(carrotSound)
     updateScoreBoard()
     if (score === CARROT_COUNT) {
       finishGame(true)
     }
   } else if (target.matches('.bug')) {
-    stopGameTimer()
     finishGame(false)
   }
+}
+
+function playSound(sound) {
+  sound.currentTime = 0
+  sound.play()
+}
+
+function stopSound(sound) {
+  sound.pause()
 }
 
 function updateScoreBoard() {
